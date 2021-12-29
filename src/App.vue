e0fe0f76f2480d9db2f604d02d8a70a1aebb9dce
rainkolwa <rainkolwa@gmail.com>
2021-12-29 19:24:19 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+const env = JSON.stringify(import.meta.env);
+const mode = import.meta.env.MODE;
+const userNodeEnv = import.meta.env.VITE_USER_NODE_ENV;
+const nodeEnv = import.meta.env.NODE_ENV;
+</script>
+
+<template>
+  <pre>{{ env }}</pre>
+  <h1>current mode: {{ mode }}</h1>
+  <h1>userNodeEnv: {{ userNodeEnv }}</h1>
+  <h1>nodeEnv: {{ nodeEnv }}</h1>
+</template>
+
+<style>
+body {
+  font-family: -apple-system, BlinkMacSystemFont, Helvetica Neue,
+    PingFangSC-Regular, PingFang SC, Microsoft YaHei, Source Han Sans SC,
+    Noto Sans CJK SC, WenQuanYi Micro Hei, sans-serif;
+}
+button,
+a {
+  @apply transition-colors duration-100;
+}
+.container {
+  @apply px-4;
+}
+</style>
