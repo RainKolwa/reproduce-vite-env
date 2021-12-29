c780a1688f0025f033b79284c7e28d78e31ddcd3
rainkolwa <rainkolwa@gmail.com>
2021-12-29 22:29:04 +0800

    fix: demo

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,11 @@
 <script setup>
-const env = JSON.stringify(import.meta.env);
+const env = JSON.stringify(import.meta.env, undefined, 2);
 const mode = import.meta.env.MODE;
-const userNodeEnv = import.meta.env.VITE_USER_NODE_ENV;
-const nodeEnv = import.meta.env.NODE_ENV;
+const prod = import.meta.env.PROD.toString();
 </script>
 
 <template>
   <pre>{{ env }}</pre>
-  <h1>current mode: {{ mode }}</h1>
-  <h1>userNodeEnv: {{ userNodeEnv }}</h1>
-  <h1>nodeEnv: {{ nodeEnv }}</h1>
+  <div>import.meta.<wbr />env.MODE: {{ mode }}</div>
+  <div>import.meta.<wbr />env.PROD: {{ prod }}</div>
 </template>
-
-<style>
-body {
-  font-family: -apple-system, BlinkMacSystemFont, Helvetica Neue,
-    PingFangSC-Regular, PingFang SC, Microsoft YaHei, Source Han Sans SC,
-    Noto Sans CJK SC, WenQuanYi Micro Hei, sans-serif;
-}
-button,
-a {
-  @apply transition-colors duration-100;
-}
-.container {
-  @apply px-4;
-}
-</style>
